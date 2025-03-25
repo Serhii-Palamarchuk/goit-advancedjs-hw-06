93fb4cc571e0a6dd75752d7a7cdd95cc11c9611a
Serhii-Palamarchuk <serhiy.palamarchuk@gmail.com>
2025-03-25 03:20:23 +0200

    Enhance TypeScript generics and type safety across multiple files

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,7 +3,7 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise () {
+function getPromise(): Promise<[string, number]> {
   return new Promise((resolve) => {
     resolve(['Text', 50]);
   });
@@ -14,4 +14,19 @@ getPromise()
   console.log(data);
 });
 
-export {};
+export { };
+
+
+/* більш детальний варіант реалізації, але так як в завданні не було вказано, що функція має бути універсальною, то варто використовувати варіант вище
+
+function getPromise<T>(): Promise<T> {
+  return new Promise<T>((resolve) => {
+    resolve(['Text', 50] as T);
+  });
+}
+
+getPromise<[string, number]>().then((data) => {
+  console.log(data);
+});
+
+*/
